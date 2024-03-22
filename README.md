b9bc8b829a08b17d4324215ddfd0f92df35120da
Manuel Ruelas <manuel.r.a@hotmail.com>
2024-03-21 19:06:43 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# marvel_heroes
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/marvel_heroes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.marvel_heroes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
