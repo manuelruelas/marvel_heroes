0e30e6582273c72bdb769215097361dc67d4b3cd
Manuel Ruelas <manuel.r.a@hotmail.com>
2024-03-21 19:10:46 -0700

    feat: create list and detail page widget

---
+++ lib/presentation/pages/hero_list/hero_list_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HeroListPage extends StatelessWidget {
+  const HeroListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
