da9355270f6e12ff9bd900894d4a597c4c83a8c5
Manuel Ruelas <manuel.r.a@hotmail.com>
2024-03-25 05:45:34 -0700

    Extract methods

--- lib/presentation/pages/hero_list/hero_list_page.dart
+++ lib/presentation/pages/hero_list/hero_list_page.dart
@@ -37,47 +37,8 @@ class HeroListPage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SearchBar(
-              controller: _searchController,
-              leading: const Icon(Icons.search),
-              hintText: "Search",
-              onChanged: (value) {
-                _controller.onSearchChange(value);
-              },
-              keyboardType: TextInputType.text,
-              trailing: [
-                IconButton(
-                  icon: const Icon(Icons.clear),
-                  onPressed: () {
-                    _searchController.clear();
-                    _controller.clearSearch();
-                  },
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Obx(
-                  () => CounterCard(
-                    title: "Listed",
-                    value: _controller.characters.length,
-                  ),
-                ),
-                Obx(
-                  () => CounterCard(
-                    title: "Total",
-                    value: _controller.charactersTotal.value,
-                  ),
-                )
-              ],
-            ),
-          ),
+          _buildSearchBar(),
+          _buildCounters(),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
@@ -98,35 +59,85 @@ class HeroListPage extends StatelessWidget {
                     );
                   }
 
-                  return ListView.builder(
-                    controller: _scrollController,
-                    itemCount: _controller.characters.length + 1,
-                    itemBuilder: (context, index) {
-                      /// Show loading indicator when user reach the last item
-                      if (index == _controller.characters.length) {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Center(
-                            child: _controller.infiniteScrollEnabled.value
-                                ? const CircularProgressIndicator()
-                                : Container(),
-                          ),
-                        );
-                      } else {
-                        final characters = _controller.characters;
-                        return CharacterItem(
-                          character: characters[index],
-                          onPressed: () {
-                            Get.toNamed("/detail",
-                                arguments: characters[index]);
-                          },
-                        );
-                      }
-                    },
-                  );
+                  return _buildCharacterList();
                 },
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ListView _buildCharacterList() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _controller.characters.length + 1,
+      itemBuilder: (context, index) {
+        /// Show loading indicator when user reach the last item
+        if (index == _controller.characters.length) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: _controller.infiniteScrollEnabled.value
+                  ? const CircularProgressIndicator()
+                  : Container(),
+            ),
+          );
+        } else {
+          final characters = _controller.characters;
+          return CharacterItem(
+            character: characters[index],
+            onPressed: () {
+              Get.toNamed("/detail", arguments: characters[index]);
+            },
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildCounters() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Obx(
+            () => CounterCard(
+              title: "Listed",
+              value: _controller.characters.length,
+            ),
+          ),
+          Obx(
+            () => CounterCard(
+              title: "Total",
+              value: _controller.charactersTotal.value,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SearchBar(
+        controller: _searchController,
+        leading: const Icon(Icons.search),
+        hintText: "Search",
+        onChanged: (value) {
+          _controller.onSearchChange(value);
+        },
+        keyboardType: TextInputType.text,
+        trailing: [
+          IconButton(
+            icon: const Icon(Icons.clear),
+            onPressed: () {
+              _searchController.clear();
+              _controller.clearSearch();
+            },
           ),
         ],
       ),
